76e396c79c37f77ac03b8071a13229ff6e75d1c1
tkcannon <taylorkcannon@gmail.com>
2021-12-23 11:26:21 -0700

    added potion functions to player

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -2,6 +2,7 @@ const Potion = require('../lib/Potion.js');
 
 test('creates a health potion object', () => {
     const potion = new Potion('health');
+    
     expect(potion.name).toBe('health');
     expect(potion.value).toEqual(expect.any(Number));
 })
